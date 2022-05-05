4dfdc88c128600eb6c171663ddc82e11201fbab9
Juan David Garzon Diaz <juandavidgarzon27@gmail.com>
2022-05-04 20:19:24 -0500

    Junite del modelo

---
+++ src/model/DataBaseTest.java
@@ -0,0 +1,38 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+class DataBaseTest {
+	DataBase db;
+	public void scneario1() {
+		db=new DataBase();
+	}
+	@Test
+	void testGeneratePeople() {
+		scneario1();
+		assertTrue(db.getFilterByCode().size()==0);
+		assertTrue(db.getFilterByName().size()==0);
+		assertTrue(db.getFilterByLastName().size()==0);
+		assertTrue(db.getFilterByFullName().size()==0);
+		try {
+			db.generatePeople(1000);
+		} catch (IOException e) {
+			fail();
+		}
+		for(int i=0;i<1000;i++) {
+			for(int j=0;j<10000;j++) {
+				//Tiempo de espera a los hilos
+			}
+		}
+		assertTrue(db.getFilterByCode().size()>0);
+		assertTrue(db.getFilterByName().size()>0);
+		assertTrue(db.getFilterByLastName().size()>0);
+		assertTrue(db.getFilterByFullName().size()>0);
+		
+	}
+
+}
